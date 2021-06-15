e58d18ca57b4030d07230f45c6968d2566d2ff23
Scott Kolmar <Kolmar.Scott@epa.gov>
2021-06-15 16:51:20 -0400

    Fixed some model properties

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -33,20 +33,21 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
     artists = db.relationship('Artist', secondary=shows, backref=db.backref('Venues', lazy=True))
-    genres = db.Column(db.String(120))
+    genres = db.Column("genres", db.Array(db.String(120)), nullable=False)
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
     seeking_description = db.Column(db.String(120))
+    shows = db.relationship('Show', backref = 'Venue', lazy=True)
 
     def __repr__(self):
       return f'<Venue ID: {self.id}, Name: {self.name}>'
@@ -56,11 +57,11 @@ class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column("genres", db.Array(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
@@ -68,6 +69,7 @@ class Artist(db.Model):
     website = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, nullable = False, default = False)
     seeking_description = db.Column(db.String(120))
+    shows = db.relationship('Show', backref = 'Artist', lazy=True)
 
     def __repr__(self):
       return f'<Artist ID: {self.id}, Name: {self.name}>'
