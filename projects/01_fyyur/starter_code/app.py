8877524e019cb257b65fb5b6aa1494055d62c24b
Scott Kolmar <Kolmar.Scott@epa.gov>
2021-06-21 10:38:49 -0400

    Finished venue/venue_id/edit route.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -479,6 +479,41 @@ def edit_venue(venue_id):
 def edit_venue_submission(venue_id):
   # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
+
+  try:
+    
+    # Instantiate form and query database for venue
+    form = VenueForm
+    venue = Venue.query.get(venue_id)
+
+    # Update values with form values
+    venue.name == form.name.data
+    venue.genres == form.genres.data
+    venue.address == form.address.data
+    venue.city == form.city.data
+    venue.state == form.state.data
+    venue.phone == form.phone.data
+    venue.website == form.website_link.data
+    venue.facebook_link == form.facebook_link.data
+    venue.seeking_talent == form.seeking_talent.data
+    venue.seeking_description == form.seeking_description.data
+    venue.image_link == form.image_link.data
+
+    # Commit changes to database and flash success message
+    db.session.commit()
+    flash('Venue ' + request.form['name'] + 'was updated.')
+
+  except:
+
+    # Rollback database session if error occurs
+    db.session.rollback()
+    flash('An error occurred, ' + request.form['name'] + 'could not be updated.')
+
+  finally:
+
+    # Close session
+    db.session.close()
+
   return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
