c60188a03b874fa764218da77d8d62f23acf9625
Scott Kolmar <Kolmar.Scott@epa.gov>
2021-06-21 09:24:02 -0400

    Finished artists route.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -303,16 +303,19 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+
+  # Query database for artists
+  artists = Artist.query.all()
+
+  # Loop through artists and append data to list
+  data = []
+  for artist in artists:
+    artist_data = {
+      "id": artist.id,
+      "name": artist.name,
+    }
+    data.append(artist_data)
+  
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
