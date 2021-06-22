547925c2c3b022139a199c15a8cef2b7f3214d2c
Scott Kolmar <Kolmar.Scott@epa.gov>
2021-06-22 17:18:09 -0400

    Constrained phone input in create artist

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -472,6 +472,13 @@ def create_artist_submission():
 
   try:
     form = ArtistForm()
+
+    # Constrain phone number to have only numbers
+    for character in form.phone.data.replace('-', ''):
+      if character not in ['0','1','2','3','4','5','6','7','8','9']:
+        flash('Phone number must be numeric!')
+        return render_template('forms/new_artist.html', form = form)
+
     artist = Artist(name = form.name.data,
     genres = form.genres.data,
     city = form.city.data,
